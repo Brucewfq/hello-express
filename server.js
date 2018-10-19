2d9a81791a5b171a8ab729ab81cf530ae962c5d3
faqun.wan <467617365@qq.com>
2018-10-19 16:43:13 +0800

    modify

--- server.js
+++ server.js
@@ -7,7 +7,7 @@
  */
 const express = require('express');
 const app  = express();
-
+const port = (process.env.PORT || 3000)
 const cookieParser = require('cookie-parser')
 // 引入mongoose模块
 const mongoose = require('mongoose');
@@ -39,4 +39,4 @@ app.get('/', function (req, res) {
     res.send('hello world');
 });
 
-app.listen('3000');
+app.listen(port);
